fe51aeb5ac23542880c1f2ebe82c8117d51bfcaf
viviann224 <viviann224@gmail.com>
2019-02-28 21:37:27 -0600

    added jawsdbauth

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'i5x1cqhq5xbqtv00.cbetxkdyhwsb.us-east-1.rds.amazonaws.c'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', '  ky2luji7k008k4vt'),
+            'username' => env('DB_USERNAME', 'jdbzb6y9qh7u4n5n'),
+            'password' => env('DB_PASSWORD', 'm58bsyd4unwdngqp'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
