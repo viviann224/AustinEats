8396c59a271842c6e1e66cbebe4bf4561fe6e7b1
viviann224 <viviann224@gmail.com>
2019-02-28 21:47:25 -0600

    added jaws auth in pgsql

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'i5x1cqhq5xbqtv00.cbetxkdyhwsb.us-east-1.rds.amazonaws.c'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', '  ky2luji7k008k4vt'),
-            'username' => env('DB_USERNAME', 'jdbzb6y9qh7u4n5n'),
-            'password' => env('DB_PASSWORD', 'm58bsyd4unwdngqp'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'i5x1cqhq5xbqtv00.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'ky2luji7k008k4vt'),
+            'username' => env('DB_USERNAME', 'jdbzb6y9qh7u4n5n'),
+            'password' => env('DB_PASSWORD', 'm58bsyd4unwdngqp'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
