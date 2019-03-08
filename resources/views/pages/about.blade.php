d97eac41f2066f322f7b79f543b6c4111dee9d07
viviann224 <viviann224@gmail.com>
2019-03-08 11:49:55 -0600

    updated  about us page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,5 +2,11 @@
 
 @section("content")
       <h1><?php echo $title; ?></h1>
-      <p>This is the about page</p>
+      <p><strong>AustinEats</strong> is a food application created to share information, personal experience, or tips at Resturants in the Austin location.</p>
+
+      <p>The application begain as a way to document nearby resturants  and alleviate time to make quick decisions on what to eat. The application now helps visitors and locals on places to dine</p>
+
+      <p><strong>AustinEats</strong> contains a series of posts that nonmembers and members can view. Members can create, edit, and remove their posts.
+        <strong>AustinEats</strong> is a laravel application which runs via <a href="https://heroku.com/apps" target="_blank">Heroku</a> deployment and uses <a href="https://elements.heroku.com/addons/jawsdb" target="_blank">JawsDB</a> SQL database.</p>
+
 @endsection
