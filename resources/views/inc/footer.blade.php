a6ff1a883f1dd69d3d3e6d816e8737d95f49dd20
viviann224 <viviann224@gmail.com>
2019-03-16 23:37:30 -0500

    updated with footer responsiveness

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,18 +1,34 @@
 <footer class="footer ">
-  <div class="container footer navbar-fixed-bottom footerBody">
+  <div class="container-fluid footer navbar-fixed-bottom footerBody">
+    <div class="row">
+  <div class="col-xs-8 col-sm-8 col-md-8">
+    &#169; Vivian Tuong Nguyen | <small>All rights reserved</small>
+  </div>
 
-
+  <div class="col-xs-4 col-sm-4 col-md-4">
+    <a href="https://viviann224.github.io/" class="socialLink" target="_blank"><img src="/images/social/logo.png" id="socialIcon"></a>
+    &nbsp <a href="https://github.com/viviann224" class="socialLink" target="_blank"><img src="/images/social/githublogo.png" id="socialIcon"></a>
+    &nbsp<a href="https://www.linkedin.com/in/viviantuongnguyen" class="socialLink" target="_blank"><img src="/images/social/linkedinlogo.png" id="socialIcon"></a>
+</div>
+    <!--<div class="row">
+      <div class="col-md-4 col-sm-4">
       &#169; Vivian Tuong Nguyen | <small>All rights reserved</small>
-      &nbsp;
+    </div>
 
 
 
+      <div class="col-md-2 col-sm-2">
+        <a href="https://viviann224.github.io/" class="socialLink" target="_blank"><img src="/images/social/logo.png" id="socialIcon"></a>
+      </div>
+      <div class="col-md-2 col-sm-2">
+        <a href="https://github.com/viviann224" class="socialLink" target="_blank"><img src="/images/social/githublogo.png" id="socialIcon"></a>
+      </div>
+      <div class="col-md-2 col-sm-2">
+      <a href="https://www.linkedin.com/in/viviantuongnguyen" class="socialLink" target="_blank"><img src="/images/social/linkedinlogo.png" id="socialIcon"></a>
+    </div>
+      <div class="col-md-2 col-sm-2">
+      </div>
 
-      <a href="https://viviann224.github.io/" class="socialLink" target="_blank"><img src="/images/social/logo.png" class="socialIcon"></a>
-      <a href="https://github.com/viviann224" class="socialLink" target="_blank"><img src="/images/social/githublogo.png" class="socialIcon"></a>
-      <a href="https://www.linkedin.com/in/viviantuongnguyen" class="socialLink" target="_blank"><img src="/images/social/linkedinlogo.png" class="socialIcon"></a>
-
-
-
+</div> -->
 </div>
 </footer>
